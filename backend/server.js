9d9fe9d95a8d75dcce1dd75d68529077c348c9eb
Thayná Alves do Nascimento Costa <166732157+thy109@users.noreply.github.com>
2026-08-24 08:43:59 -0300

    Fix typo in process.env.PORT assignment

--- backend/server.js
+++ backend/server.js
@@ -187,7 +187,7 @@ app.get("/medicacoes", (req, res) => {
 });
 
 // START
-  const PORT = preocess.env.PORT 
+  const PORT = process.env.PORT 
                  || 3000;
   app.listen(PORT, () => {
     console.log(`Porta ${PORT}`)});
